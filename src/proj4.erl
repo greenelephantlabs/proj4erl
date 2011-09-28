a1305c880599c0206ba0a939c1c3cfb975fa239d
Gleb Peregud <gleber.p@gmail.com>
2011-09-28 15:09:54 +0200

    add specs

--- src/proj4.erl
+++ src/proj4.erl
@@ -1,14 +1,18 @@
 -module(proj4).
 -export([init/1, transform/3, get_def/1]).
 
+-opaque cd() :: binary().
+-type point() :: {float(), float()} | {float(), float(), float()}.
+
+-spec init/1 :: (string()) -> {'ok', cd()} | {'error', string()}.
+-spec transform/3 :: (cd(), cd(), point()) -> {'ok', point()}.
+-spec get_def/1 :: (cd()) -> string().
+
 -include_lib("eunit/include/eunit.hrl").
 
 -on_load(load_nif/0).
 
-load_nif() ->
-    Dir = filename:dirname(code:where_is_file("proj4erl.app")),
-    Path = filename:join([Dir, "..", "priv", "proj4erl_nif"]),
-    erlang:load_nif(Path, 0).
+
 
 init(_Args) ->
     nif_error(?LINE).
@@ -23,6 +27,14 @@ nif_error(Line) ->
     exit({nif_not_loaded,module,?MODULE,line,Line}).
 
 
+
+load_nif() ->
+    Dir = filename:dirname(code:where_is_file("proj4erl.app")),
+    Path = filename:join([Dir, "..", "priv", "proj4erl_nif"]),
+    erlang:load_nif(Path, 0).
+
+
+
 init_test() ->
     proj4:init(["proj=merc", "ellps=clrk66", "lat_ts=33"]).
 
@@ -35,3 +47,5 @@ wgs84_2180_test() ->
     {ok, P2} = proj4:transform(WGS84, CRS2180, P),
     io:format("Transformed from '~s' to '~s': ~p~n", [proj4:get_def(WGS84), proj4:get_def(CRS2180), P2]),
     {639951.5695094677, 486751.7840663176} = P2.
+
+
