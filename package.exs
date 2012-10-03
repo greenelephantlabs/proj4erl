8d0e1cd5e75e26364090801835e14b095cd473bb
Gleb Peregud <gleber.p@gmail.com>
2012-10-03 11:51:21 +0200

    add package.exs

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "proj4erl", description: "PROJ.4 bidings for Erlang using NIFs",
+                 version: "0.5.0", keywords: ["proj4", "projection", "conversion", "geo"], 
+                 maintainers: [[name: "Gleb Peregud", 
+                                email: "gleber.p@gmail.com"]],
+                 repositories: [[github: "greenelephantlabs/proj4erl"]])
